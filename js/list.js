9000339afea04e904876b085e9ae2ada978a7907
breakcore11 <82821209+breakcore11@users.noreply.github.com>
2021-04-19 17:56:16 +0300

    Update list.js

--- js/list.js
+++ js/list.js
@@ -3,118 +3,98 @@ const d = {
 		/*=================================================================================*/
 		{
 			"vids": [
-
-
-				
-
+			
       ],
-			"name": "To My fffingers I",
-			"author": "KrmaL",
-			"more": "none",
-			"id": "9",
-			"pass": "696969",
-			"percentToQualify": "76",
-			"verificationVid": "https://www.youtube.com/watch?v=SLLkyqwUmMg",
-			"key": 0
+			"name": "To my fffingers I",
+                       "author": "KrmaL",
+                       "more": "none",
+                       "id": 9,
+                       "pass": "696969",
+                       "percentToQualify": 78,
+                       "verificationVid": "https://www.youtube.com/watch?v=SLLkyqwUmMg",
+                       "key": 0
 		},
+		/*=================================================================================*/
+		{
+		"vids": [
+
+			],
+                       "name": "Nebula",
+                       "author": "Nukie",
+                       "more": "none",
+                       "id": 15,
+                       "pass": "696969",
+                       "percentToQualify": 81,
+                       "verificationVid": "https://www.youtube.com/watch?v=rh-FAxU610c",
+                       "key": 0
+                },
+		/*=================================================================================*/
+		{
+			"vids": [
+	
+				],
+						   "name": "Daddy",
+						   "author": "Nukie",
+						   "more": "none",
+						   "id": 19,
+						   "pass": "Free Copy",
+						   "percentToQualify": 100,
+						   "verificationVid": "https://www.youtube.com/watch?v=fmtU4VRSQuE",
+						   "key": 0
+					},
+			/*=================================================================================*/
+			{
+				"vids": [
 		
-/*=================================================================================*/
-{
-	"vids": [
+					],
+							   "name": "Atmosphere",
+							   "author": "Nukie",
+							   "more": "none",
+							   "id": 12,
+							   "pass": "Free Copy",
+							   "percentToQualify": 100,
+							   "verificationVid": "https://www.youtube.com/watch?v=YY7dxmadi7Y",
+							   "key": 0
+						},
+				/*=================================================================================*/
+				{
+					"vids": [
+			
+						],
+								   "name": "SuccY",
+								   "author": "Nukie",
+								   "more": "none",
+								   "id": 16,
+								   "pass": "555555",
+								   "percentToQualify": 100,
+								   "verificationVid": "",
+								   "key": 0
+							},
+					/*=================================================================================*/
+					{
+						"vids": [
+				
+							],
+									   "name": "Awakening",
+									   "author": "Nukie",
+									   "more": "none",
+									   "id": 10,
+									   "pass": "Free Copy",
+									   "percentToQualify": 100,
+									   "verificationVid": "https://www.youtube.com/watch?v=FZIhhWkrs-c",
+									   "key": 0
+								},
+						/*=================================================================================*/
 
 
-		
-
-],
-	"name": "Nebula",
-	"author": "Nukie",
-	"more": "none",
-	"id": "15",
-	"pass": "696969",
-	"percentToQualify": "81",
-	"verificationVid": "https://www.youtube.com/watch?v=rh-FAxU610c",
-	"key": 0
-},
-
-/*=================================================================================*/
-{
-	"vids": [
 
 
-		
-
-],
-	"name": "Daddy",
-	"author": "Nukie",
-	"more": "none",
-	"id": "19",
-	"pass": "Free Copy",
-	"percentToQualify": "100",
-	"verificationVid": "https://www.youtube.com/watch?v=fmtU4VRSQuE",
-	"key": 0
-},
-
-/*=================================================================================*/
-{
-	"vids": [
-
-
-		
-
-],
-	"name": "Atmosphere",
-	"author": "Nukie",
-	"more": "none",
-	"id": "12",
-	"pass": "Free Copy",
-	"percentToQualify": "100",
-	"verificationVid": "https://www.youtube.com/watch?v=YY7dxmadi7Y",
-	"key": 0
-},
-
-/*=================================================================================*/
-{
-	"vids": [
-
-
-		
-
-],
-	"name": "SuccY",
-	"author": "Nukie",
-	"more": "none",
-	"id": "16",
-	"pass": "555555",
-	"percentToQualify": "100",
-	"verificationVid": "",
-	"key": 0
-},
-
-/*=================================================================================*/
-{
-	"vids": [
-
-
-		
-
-],
-	"name": "Awakening",
-	"author": "Nukie",
-	"more": "none",
-	"id": "10",
-	"pass": "Free Copy",
-	"percentToQualify": "100",
-	"verificationVid": "https://www.youtube.com/watch?v=FZIhhWkrs-c",
-	"key": 0
-},
-
-/*only modify version with every MAJOR update (you can always check update number at https://github.com/electroflameofficial/gdshittylist/releases*/
 
 	],
 	"version": [
 		1,
-		0,
-		0
+		5,
+		1
 	]
 };
 
