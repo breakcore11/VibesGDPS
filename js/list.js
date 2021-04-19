2e1e0ff5886aec685d06e281c573ff431aa1fd97
demonlist <76852729+demonlist@users.noreply.github.com>
2021-04-19 16:39:20 +0300

    first commit

---
+++ js/list.js
@@ -0,0 +1,46 @@
+const d = {
+	"list": [
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "Nukie",
+					"link": "https://www.youtube.com/watch?v=HlDaQYugKmc",
+					"percent": "100",
+					"hz": "360hz"
+				},
+				{
+					"user": "test",
+					"link": "https://www.youtube.com/watch?v=iohamJvra9k",
+					"percent": "78",
+					"hz": "3hz"
+				},
+				
+				
+
+      ],
+			"name": "To My fffingers I",
+			"author": "KrmaL",
+			"more": "none",
+			"id": "9",
+			"pass": "696969",
+			"percentToQualify": "76",
+			"verificationVid": "https://www.youtube.com/watch?v=SLLkyqwUmMg",
+			"key": 0
+		},
+		/*=================================================================================*/
+
+
+
+
+/*only modify version with every MAJOR update (you can always check update number at https://github.com/electroflameofficial/gdshittylist/releases*/
+
+	],
+	"version": [
+		1,
+		0,
+		0
+	]
+};
+
+const list = d.list;const version = d.version;
